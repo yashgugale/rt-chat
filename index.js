4d55f5b46be5f4580333d15829a9c93dbb4aa34a
yashgugale <yashgugale@gmail.com>
2023-02-10 18:46:51 -0600

    basic app complete

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const socket = require('socket.io');
+
 var app = express()
 
-app.listen(4000, function() {
+var server = app.listen(4000, function() {
     console.log("Listening on port 4000!");
 })
+
+app.use(express.static("public"));
+
+var socketServer = socket(server);
+
+socketServer.on("connection", function(socket) {
+
+    socket.on('sendingMessage', function(data) {
+        socketServer.emit('broadcastMessage', data);
+    });
+
+    console.log("Websocket connected", socket.id);
+});
